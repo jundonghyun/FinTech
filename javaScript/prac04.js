8201fbcebdbcddc55df3ca2475d08d5739895c8b
전동현 <jundonghyun>
2022-02-22 13:25:41 +0900

    자바스크립트 배열종류

---
+++ javaScript/prac04.js
@@ -0,0 +1,23 @@
+//자바스크립트 배열
+let array = ["Apple", "Banana", "Pineapple"];
+
+let car1 = "ioniq";
+let car2 = "sonata";
+let car3 = "tesla";
+
+let carAray = [car1, car2, car3];
+
+//기본적인 for
+for(let i = 0; i < carAray.length; i++){
+
+}
+
+//좀더 편한 for
+for(fruit of array){
+    
+}
+
+//react에서 많이 사용하는 for
+array.map((car) => {
+    console.log(car)
+})
